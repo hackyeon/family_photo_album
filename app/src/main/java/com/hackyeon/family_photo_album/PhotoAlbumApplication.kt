09a2ce28de9ea62720e545731e64db37dd8099e0
hackyeon <skek933@gmail.com>
2022-06-21 23:50:57 +0900

    종속성 추가
    종속성 추가

---
+++ app/src/main/java/com/hackyeon/family_photo_album/PhotoAlbumApplication.kt
@@ -0,0 +1,10 @@
+package com.hackyeon.family_photo_album
+
+import android.app.Application
+
+class PhotoAlbumApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
